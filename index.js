565b179cb8fa14a5748e6c91e573434c7ed19847
Mayer St Germain <70457671+mayer509@users.noreply.github.com>
2022-02-24 14:45:58 -0400

    more

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import Paddle from "/paddle";
+import inputHandler from "/input";
 
 let canvas = document.getElementById("gameScreen");
 let ctx = canvas.getContext("2d");
@@ -6,8 +7,23 @@ let ctx = canvas.getContext("2d");
 const GAME_WIDTH = 800;
 const GAME_HEIGHT = 600;
 
-ctx.clearRect(0, 0, 800, 600);
-
 let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
 
+new inputHandler(paddle);
+
 paddle.draw(ctx);
+
+let lastTime = 0;
+
+function gameLoop(timeStamp) {
+  let deltaTime = timeStamp - lastTime;
+  lastTime = timeStamp;
+
+  ctx.clearRect(0, 0, 800, 600);
+
+  paddle.update(deltaTime);
+  paddle.draw(ctx);
+
+  requestAnimationFrame(gameLoop);
+}
+gameLoop();
--- paddle.js
+++ paddle.js
@@ -3,13 +3,30 @@ export default class Paddle {
     this.width = 150;
     this.height = 20;
 
+    this.maxSpeed = 7;
+    this.speed = 2;
+
     this.position = {
       x: gameWidth / 2 - this.width / 2,
       y: gameHeight - this.height - 10
     };
   }
+
+  moveLeft() {
+    this.speed = -this.maxSpeed;
+  }
+  moveRight() {
+    this.speed = this.maxSpeed;
+  }
+
   draw(ctx) {
     ctx.fillStyle = "#0ff";
     ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+
+  update(deltaTime) {
+    if (!deltaTime) return;
+
+    this.position.x += 5 / deltaTime;
   }
 }
