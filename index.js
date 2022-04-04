f13a02ce5bd29deef2e4c049c54b7a266d6c0ea1
Mayer St Germain <70457671+mayer509@users.noreply.github.com>
2022-04-04 09:48:47 -0700

    Update from main

--- index.js
+++ index.js
@@ -1,28 +1,44 @@
-import Paddle from "/paddle";
-import inputHandler from "/input";
+let factorValue = 1;
 
-let canvas = document.getElementById("gameScreen");
-let ctx = canvas.getContext("2d");
+let fontFamily = "65px 'Gill Sans Ultra', sans-serif";
 
-const GAME_WIDTH = 800;
-const GAME_HEIGHT = 600;
+let ctx;
 
-let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
-
-new inputHandler(paddle);
-
-paddle.draw(ctx);
-
-let lastTime = 0;
-
-function gameLoop(timeStamp) {
-  let deltaTime = timeStamp - lastTime;
-  lastTime = timeStamp;
-
-  paddle.update(deltaTime);
-  paddle.draw(ctx);
-  ctx.clearRect(0, 0, 800, 600);
-
-  requestAnimationFrame(gameLoop);
+function init() {
+  ctx = document.getElementById("canvas").getContext("2d");
+  ctx.font = fontFamily;
+  ctx.save();
+  dologo();
 }
-gameLoop();
+init();
+
+function dologo() {
+  let offsetY = 80;
+
+  ctx.restore();
+  ctx.save();
+
+  ctx.clearRect(0, 0, 600, 400);
+  ctx.scale(factorValue, factorValue);
+  ctx.fillText("HTML", 31, 60);
+  ctx.translate(0, offsetY);
+
+  ctx.fillStyle = "#e34c26";
+  ctx.beginPath();
+  ctx.moveTo(39, 250);
+  ctx.lineTo(17, 0);
+  ctx.lineTo(262, 0);
+  ctx.lineTo(239, 250);
+  ctx.lineTo(139, 278);
+  ctx.closePath();
+  ctx.fill();
+
+  ctx.fillStyle = "#f06529";
+  ctx.beginPath();
+  ctx.moveTo(139, 257);
+  ctx.lineTo(220, 234);
+  ctx.lineTo(239, 20);
+  ctx.lineTo(139, 20);
+  ctx.closePath();
+  ctx.fill();
+}
